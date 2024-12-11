beb06a4dfb60eadbe631601120126ab182bbd490
pnkrc <cpankracgurin@gmail.com>
2024-12-11 00:49:15 -0300

    Add datepicker

---
+++ src/components/FormReservaInternacion.tsx
@@ -0,0 +1,9 @@
+import { DatePicker } from "@/components/ui/date-picker";
+
+export function FormReservaInternacion() {
+  return (
+    <div className="flex flex-col  w-full h-full">
+      <DatePicker />
+    </div>
+  );
+}
